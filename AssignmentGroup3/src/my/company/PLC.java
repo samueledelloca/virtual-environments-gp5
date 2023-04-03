0b706837133cdaea7c99c554503650874d941de7
Samuele Dell'Oca <samuele.delloca@supsi.ch>
2023-04-03 15:10:51 +0200

    Splitted conveyor A

--- AssignmentGroup3/src/my/company/PLC.java
+++ AssignmentGroup3/src/my/company/PLC.java
@@ -5,6 +5,8 @@
  */
 package my.company;
 
+import com.ttsnetwork.modules.standard.ISource;
+import com.ttsnetwork.modules.standard.ParametricSource2;
 import com.ttsnetwork.modules.standard.ProgrammableLogics;
 
 
@@ -13,10 +15,21 @@ import com.ttsnetwork.modules.standard.ProgrammableLogics;
  * @author samu
  */
 public class PLC extends ProgrammableLogics {
+//    
+//    public ParametricSource2 SA;
+//    ISource srcCmd;
 
     @Override
     public void onInit() {
-        
+//        srcCmd = SA.createSkill(module);
+//        
+//        schedule.startSerial();
+//        {
+//            schedule.waitTime(4000);
+//            srcCmd.create(null);
+//            schedule.waitTime(4000);
+//        }
+//        schedule.end();
     }
 
 }
