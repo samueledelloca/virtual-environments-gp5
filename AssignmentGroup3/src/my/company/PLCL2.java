8ae5b82100bbe43fc7538e0a434472df8a64863b
Samuele Dell'Oca <samuele.delloca@supsi.ch>
2023-05-08 15:51:05 +0200

    Welding on second line

--- AssignmentGroup3/src/my/company/PLCL2.java
+++ AssignmentGroup3/src/my/company/PLCL2.java
@@ -9,6 +9,8 @@ import com.ttsnetwork.commands.Transform;
 import com.ttsnetwork.core.AWJoint;
 import com.ttsnetwork.core.AWRS;
 import com.ttsnetwork.modules.standard.BoxUtils;
+import com.ttsnetwork.modules.standard.ConveyorArc;
+import com.ttsnetwork.modules.standard.ConveyorLine;
 import com.ttsnetwork.modules.standard.ConveyorLine2;
 import com.ttsnetwork.modules.standard.IConveyorCommands;
 import com.ttsnetwork.modules.standard.IRobotCommands;
@@ -26,18 +28,26 @@ public class PLCL2 extends ProgrammableLogics {
 
     public ConveyorBox dBox;
     public ConveyorBox abd3Box;
+    public ConveyorBox abd4Box;
 
     public Robot6DOF2 R4;
+    public Robot6DOF2 R5;
+
     public IRobotCommands r4Cmd;
+    public IRobotCommands r5Cmd;
 
     public ConveyorLine2 D;
     public ConveyorLine2 ABD3;
+    public ConveyorLine2 ABD4;
 
     public IConveyorCommands dCmd;
     public IConveyorCommands abd3Cmd;
+    public IConveyorCommands abd4Cmd;
+    public IConveyorCommands abd5Cmd;
 
     public ISensorProvider dSen;
     public ISensorProvider abd3Sen;
+    public ISensorProvider abd4Sen;
 
     private void dBoxArrived(SensorCatch sc) {
         // record the arrived box
@@ -46,7 +56,7 @@ public class PLCL2 extends ProgrammableLogics {
         {
             // immediately stop conveyor
             dCmd.speedSet(0);
-            doAssembly();
+            doAbdAssembly();
         }
         schedule.end();
     }
@@ -56,20 +66,25 @@ public class PLCL2 extends ProgrammableLogics {
         schedule.startSerial();
         {
             abd3Cmd.speedSet(0);
-            doAssembly();
+            doAbdAssembly();
         }
         schedule.end();
     }
 
-    private void doAssembly() {
+    private void abd4BoxArrived(SensorCatch sc) {
+        abd4Box = sc.box;
+        schedule.startSerial();
+        {
+            abd4Cmd.speedSet(0);
+            doAbdWelding();
+        }
+        schedule.end();
+    }
+
+    private void doAbdAssembly() {
         if (dBox != null && abd3Box != null) {
             schedule.startSerial();
             {
-//                AWRS entity = abd3Box.entity;
-//                while(entity.getChildren() == null) {
-//                    entity = entity.getChildren().get(0).getChild();
-//                }
-//                System.out.println(entity.getFrame(null));
                 Transform dTargetOffset = BoxUtils.targetOffset(dBox, 0, 0, BoxUtils.zSize(dBox) * 2, 0, 0, 0);
                 r4Cmd.move(dTargetOffset, 2000);
                 r4Cmd.moveLinear(BoxUtils.targetTop(dBox), 2000);
@@ -92,9 +107,25 @@ public class PLCL2 extends ProgrammableLogics {
         }
     }
 
+    private void doAbdWelding() {
+        schedule.startSerial();
+        {
+            Transform abd4TargetOffset = BoxUtils.targetOffset(abd4Box, 0, 0, BoxUtils.zSize(abd4Box) * 4, 0, 0, 180);
+            Transform abd4TopTargetOffset = BoxUtils.targetOffset(abd4Box, 0, 0, BoxUtils.zSize(abd4Box) * 3, 0, 0, 180);
+            r5Cmd.move(abd4TargetOffset, 2000);
+
+            r5Cmd.moveLinear(abd4TopTargetOffset, 2000);
+            r5Cmd.moveLinear(abd4TargetOffset, 2000);
+            abd4Cmd.speedSet(1);
+        }
+        schedule.end();
+        abd4Box = null;
+    }
+
     @Override
     public void onInit() {
         r4Cmd = R4.create(module);
+        r5Cmd = R5.create(module);
 
         dCmd = D.createCommands(module);
         dSen = D.createSensors(module);
@@ -102,8 +133,12 @@ public class PLCL2 extends ProgrammableLogics {
         abd3Cmd = ABD3.createCommands(module);
         abd3Sen = ABD3.createSensors(module);
 
+        abd4Cmd = ABD4.createCommands(module);
+        abd4Sen = ABD4.createSensors(module);
+
         dSen.registerOnSensors(this::dBoxArrived, "S1D");
         abd3Sen.registerOnSensors(this::abd3BoxArrived, "S1ABD3");
+        abd4Sen.registerOnSensors(this::abd4BoxArrived, "S1ABD4");
     }
 
 }
--- AssignmentGroup3/src/my/company/PLCL3.java
+++ AssignmentGroup3/src/my/company/PLCL3.java
@@ -5,7 +5,6 @@
  */
 package my.company;
 
-
 import com.ttsnetwork.modules.standard.ConveyorLine2;
 import com.ttsnetwork.modules.standard.IConveyorCommands;
 import com.ttsnetwork.modules.standard.IRobotCommands;
@@ -23,6 +22,8 @@ import org.apache.commons.math3.distribution.RealDistribution;
  */
 public class PLCL3 extends ProgrammableLogics {
 
+    public ConveyorLine2 ABC;
+    public ConveyorLine2 ABD5;
     public ConveyorLine2 E1;
     public ConveyorLine2 E2;
     public ConveyorLine2 E3;
@@ -31,32 +32,35 @@ public class PLCL3 extends ProgrammableLogics {
     public Pusher P2;
     public Pusher P3;
 
+    private ISensorProvider s1abcSens;
+    private ISensorProvider s1abd5Sens;
     private ISensorProvider s1e1Sens;
-    private ISensorProvider s2e1Sens;
     private ISensorProvider s1e2Sens;
     private ISensorProvider s2e2Sens;
     private ISensorProvider s1e4Sens;
     private ISensorProvider s2e4Sens;
-    
+
+    private IConveyorCommands abcCmd;
+    private IConveyorCommands abd5Cmd;
     private IConveyorCommands e1Cmd;
     private IConveyorCommands e2Cmd;
     private IConveyorCommands e3Cmd;
     private IConveyorCommands e4Cmd;
-    
+
     private IRobotCommands r3Cmd;
-    
+
     private ConveyorBox boxOn1;
     private ConveyorBox boxOn2;
     private ConveyorBox boxOn3;
     private ConveyorBox boxOn4;
-    
+
     private RealDistribution distribution;
-    
-     
+
     private void Sensor2(SensorCatch sc) {
 //      // If there are boxes on1 allora ferma il conveyor 
-        if(boxOn1 != null)
-        boxOn1 = sc.box;
+        if (boxOn1 != null) {
+            boxOn1 = sc.box;
+        }
         schedule.startSerial();
         {
             // immediately stop conveyor
@@ -65,10 +69,12 @@ public class PLCL3 extends ProgrammableLogics {
         }
         schedule.end();
     }
-     private void Sensor4(SensorCatch sc) {
+
+    private void Sensor4(SensorCatch sc) {
 //      // If there are boxes on1 allora ferma il conveyor 
-        if(boxOn2 != null)
-        boxOn1 = sc.box;
+        if (boxOn2 != null) {
+            boxOn1 = sc.box;
+        }
         schedule.startSerial();
         {
             // immediately stop conveyor
@@ -77,89 +83,81 @@ public class PLCL3 extends ProgrammableLogics {
         }
         schedule.end();
     }
-    
-    private void doWelding(){
+
+    private void doWelding() {
         // Welding can be performed only if the box is present
-        if(boxOn1 != null){
+        if (boxOn1 != null) {
             // execute the series of assembly operations
             schedule.startSerial();
             {
-              
-              r3Cmd.move(driver.getFrameTransform("root.Welding"), 2000);
-              
-              schedule.waitTime(15000);   
+
+                r3Cmd.move(driver.getFrameTransform("root.Welding"), 2000);
+
+                schedule.waitTime(15000);
             }
             schedule.end();
             // forget the operated boxes
             boxOn1 = null;
-            
+
         }
     }
-    
-    
-    private void doThermalTreatment(){
+
+    private void doThermalTreatment() {
         schedule.startSerial();
-            {
-              
-                
-        
-           
-            }
-            schedule.end();
+        {
+
+        }
+        schedule.end();
     }
-    
-    
-       
-    
-     private void doQualityCheck1(){
+
+    private void doQualityCheck1() {
         // Welding can be performed only if the box is present
-        if(boxOn2 != null){
+        if (boxOn2 != null) {
             // execute the series of quality check operations
             schedule.startSerial();
             {
-              
-              schedule.waitTime(30000);   
+
+                schedule.waitTime(30000);
             }
             schedule.end();
             // forget the operated boxes
             boxOn2 = null;
         }
     }
-    
-      private void doQualityCheck2(){
+
+    private void doQualityCheck2() {
         // Welding can be performed only if the box is present
-        if(boxOn4 != null){
+        if (boxOn4 != null) {
             // execute the series of quality check operations
             schedule.startSerial();
             {
-              
-                      schedule.waitTime(30000);   
+
+                schedule.waitTime(30000);
             }
             schedule.end();
             // forget the operated boxes
             boxOn4 = null;
         }
     }
+
     @Override
     public void onInit() {
-        
+
         e1Cmd = E1.createCommands(module);
         e2Cmd = E2.createCommands(module);
         e3Cmd = E3.createCommands(module);
         e4Cmd = E4.createCommands(module);
-        
+
         s1e1Sens = E1.createSensors(module);
         s1e2Sens = E1.createSensors(module);
         s1e2Sens = E2.createSensors(module);
         s2e2Sens = E2.createSensors(module);
         s1e4Sens = E4.createSensors(module);
         s2e4Sens = E4.createSensors(module);
-        
+
         r3Cmd = R3.create(module);
-        
+
       // s1e1Sens.registerOnSensors(this::Sensor1, "S1E1");
-       
-        
         schedule.startSerial();
         {
             schedule.waitTime(1000);
